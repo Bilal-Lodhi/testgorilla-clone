972a289733c2b0113f436909db67acbfe15a43f9
Bilal-Lodhi <braza4715@gmail.com>
2026-05-06 14:18:30 +0500

    feat(utils): add before-unload abstract class for web exit prompts

---
+++ frontend/test_gorilla/lib/core/utils/before_unload.dart
@@ -0,0 +1,5 @@
+import 'before_unload_stub.dart'
+    if (dart.library.js) 'before_unload_handler_web.dart';
+
+export 'before_unload_stub.dart'
+    if (dart.library.js) 'before_unload_handler_web.dart';
